75ffb6d11a398ab0a838dc3406df7838931274c1
Remco Bloemen <remco@coblue.eu>
2016-02-01 16:35:56 +0100

    Add Interval

---
+++ src/Interval.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <iostream>
+#include <limits>
+
+// The represented interval is [base · 2⁻⁶⁴, (base + range + 1) · 2⁻⁶⁴)
+class Interval {
+public:
+	typedef std::uint64_t uint64;
+	static constexpr int bits = 8 * sizeof(uint64);
+	static constexpr uint64 max = std::numeric_limits<uint64>::max();
+	static constexpr uint64 msb = 1UL << (bits - 1);
+	
+	Interval() { }
+	explicit Interval(double probability);
+	Interval(uint64 base, uint64 range);
+	
+	uint64 base = 0;
+	uint64 range = max;
+	
+	bool operator==(const Interval& other) const;
+	bool operator!=(const Interval& other) const { return !(*this == other); }
+	bool is_normalized() const;
+	bool is_goofy() const;
+	double probability() const;
+	double entropy() const;
+	bool includes(uint64 value) const;
+	bool includes(const Interval& interval) const;
+	bool overlaps(const Interval& interval) const;
+	
+	void update(const Interval& symbol, bool* carry = nullptr);
+	std::vector<bool> normalize();
+};
+
+std::ostream& operator<<(std::ostream& out, const Interval& interval);
