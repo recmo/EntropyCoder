26c0093425a416871211466ab9e919c849e61c20
Remco Bloemen <remco@coblue.eu>
2016-02-02 12:13:28 +0100

    Add div128 and rename mul128

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,7 +1,8 @@
 #include "Utilities.h"
 #include <iomanip>
+typedef std::uint64_t uint64;
 
-std::uint64_t count_leading_zeros(std::uint64_t value)
+uint64 count_leading_zeros(uint64 value)
 {
 	// Change the behaviour on all zeros
 	if(value == 0) {
@@ -10,45 +11,62 @@ std::uint64_t count_leading_zeros(std::uint64_t value)
 	return __builtin_clzll(value);
 }
 
-std::pair<std::uint64_t, std::uint64_t> mulq_emu(std::uint64_t a, std::uint64_t b)
+std::pair<uint64, uint64> add128(uint64 h, uint64 l, uint64 n)
 {
-	using std::uint64_t;
-	const uint64_t u1 = (a & 0xffffffff);
-	const uint64_t v1 = (b & 0xffffffff);
-	uint64_t t = (u1 * v1);
-	const uint64_t w3 = (t & 0xffffffff);
-	uint64_t k = (t >> 32);
+	l += n;
+	h += (l < n) ? 1 : 0;
+	return std::make_pair(h, l);
+}
+
+std::pair<uint64, uint64> mul128_emu(uint64 a, uint64 b);
+
+std::pair<uint64, uint64> mul128(uint64 a, uint64 b)
+{
+	#ifdef __LP64__
+		// h 2⁶⁴ + l = a b
+		uint64 h, l;
+		asm("mulq %3"
+			: "=a"(l), "=d"(h)
+			: "a"(a), "rm"(b));
+		return std::make_pair(h, l);
+	#else
+		return mul128_emu(a, b);
+	#endif
+}
+
+std::pair<uint64, uint64> div128_emu(uint64 h, uint64 l, uint64 d);
+
+std::pair<uint64, uint64> div128(uint64 h, uint64 l, uint64 d)
+{
+	#ifdef __LP64__
+		uint64 q, r;
+		asm("divq %4"
+			: "=a"(q), "=d"(r)
+			: "d"(h), "a"(l), "rm"(d));
+		return std::make_pair(q, r);
+	#else
+		return div128_emu(h, l, d);
+	#endif
+}
+
+std::pair<uint64, uint64> mul128_emu(uint64 a, uint64 b)
+{
+	const uint64 u1 = (a & 0xffffffff);
+	const uint64 v1 = (b & 0xffffffff);
+	uint64 t = (u1 * v1);
+	const uint64 w3 = (t & 0xffffffff);
+	uint64 k = (t >> 32);
 	
 	a >>= 32;
 	t = (a * v1) + k;
 	k = (t & 0xffffffff);
-	uint64_t w1 = (t >> 32);
+	uint64 w1 = (t >> 32);
 	
 	b >>= 32;
 	t = (u1 * b) + k;
 	k = (t >> 32);
 	
-	const uint64_t h = (a * b) + w1 + k;
-	const uint64_t l = (t << 32) + w3;
-	return std::make_pair(h, l);
-}
-
-std::pair<std::uint64_t, std::uint64_t> mulq(std::uint64_t a, std::uint64_t b)
-{
-	using std::uint64_t;
-	#ifdef __LP64__
-		// h 2⁶⁴ + l = a b
-		uint64_t h, l;
-		asm("mulq %3" : "=a"(l), "=d"(h) : "a"(a), "rm"(b));
-		return std::make_pair(h, l);
-	#else
-		return mulq_emu(a, b);
-	#endif
-}
-
-std::pair<std::uint64_t, std::uint64_t> add128(std::uint64_t h, std::uint64_t l, std::uint64_t n)
-{
-	l += n;
-	h += (l < n) ? 1 : 0;
+	const uint64 h = (a * b) + w1 + k;
+	const uint64 l = (t << 32) + w3;
 	return std::make_pair(h, l);
 }
--- src/Utilities.h
+++ src/Utilities.h
@@ -5,10 +5,11 @@
 /// @returns The number of zeros in value starting from MSB 
 std::uint64_t count_leading_zeros(std::uint64_t value);
 
-/// @returns  ⌊a·b·2⁻⁶⁴⌋ and a·b mod 2⁶⁴
-std::pair<std::uint64_t, std::uint64_t> mulq(std::uint64_t a, std::uint64_t b);
-
 /// @returns  ⌊(h·2⁶⁴ + l + n) 2⁻⁶⁴⌋ and (h·2⁶⁴ + l + n) mod 2⁶⁴
 std::pair<std::uint64_t, std::uint64_t> add128(std::uint64_t h, std::uint64_t l, std::uint64_t n);
 
+/// @returns  ⌊a·b·2⁻⁶⁴⌋ and a·b mod 2⁶⁴
+std::pair<std::uint64_t, std::uint64_t> mul128(std::uint64_t a, std::uint64_t b);
 
+/// @returns  ⌊((h·2⁶⁴ + l) / d⌋ and (h·2⁶⁴ + l) mod d
+std::pair<std::uint64_t, std::uint64_t> div128(std::uint64_t h, std::uint64_t l, std::uint64_t d);
