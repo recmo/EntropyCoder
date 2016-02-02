2c4685e5164748868d2f20bae98251e37b351ea2
Remco Bloemen <remco@coblue.eu>
2016-02-02 12:18:33 +0100

    Use Interval::descale in EntropyReader

--- src/EntropyReader.h
+++ src/EntropyReader.h
@@ -9,11 +9,12 @@ public:
 	EntropyReader(std::istream& input);
 	
 	bool eof() const;
-	uint64 read() const { return current.base; }
+	uint64 read() const;
 	void read(const Interval& symbol);
 	
 private:
 	BinaryReader br;
 	Interval current;
+	Interval::uint64 value;
 	uint eof_count = 0;
 };
