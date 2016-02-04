e0c4bb1ce71a8ec23951c3624cd5b47faa3c98e6
Remco Bloemen <remco@coblue.eu>
2016-02-04 10:55:18 +0100

    More tests

--- src/EncodeDecode.test.cpp
+++ src/EncodeDecode.test.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <cassert>
 #include <vector>
+#include "Test.h"
 
 SUITE(EncodeDecode) {
 
@@ -12,143 +13,68 @@ typedef std::uint64_t uint64;
 typedef std::vector<bool> Input;
 typedef std::vector<Input> Inputs;
 
-// Generate all the symbols bit strings of length n
-Inputs generate(uint length)
+TEST(EncodeDecode)
 {
-	Inputs result;
-	assert(length < 24);
-	const uint64 max = 1UL << length;
-	result.reserve(max);
-	for(uint64 bits = 0; bits < max; ++bits) {
-		Input input;
-		input.reserve(length);
-		for(uint64 bit = 1; bit < max; bit <<= 1) {
-			input.push_back(bits & bit);
-		}
-		result.push_back(input);
-	}
-	return result;
-}
-
-void test(const Inputs& inputs, double probability)
-{
-	// Model
-	const Interval intervalTrue{probability};
-	const Interval intervalFalse{
-		intervalTrue.base + intervalTrue.range + 1,
-		Interval::max - intervalTrue.range - 1
-	};
-	
-	// Run through data
-	for(const Input& input: inputs) {
+	const Inputs inputs = generate_bitstrings(5);
+	std::stringstream buffer;
+	for(double p: probabilities) {
 		
-		// Encode
-		std::stringstream buffer;
-		{
-			EntropyWriter ew(buffer);
-			for(bool bit: input) {
-				ew.write(bit ? intervalTrue : intervalFalse);
+		// Model
+		const Interval intervalTrue{p};
+		const Interval intervalFalse{
+			intervalTrue.base + intervalTrue.range + 1,
+			Interval::max - intervalTrue.range - 1
+		};
+		
+		for(const Input& input: inputs) {
+			
+			std::cerr << p;
+			for(bool bit: input)
+				std::cerr << (bit ? " true" : " false");
+			std::cerr << std::endl;
+			
+			double entropy = 0.0;
+			
+			// Encode
+			buffer = std::stringstream{};
+			{
+				EntropyWriter ew(buffer);
+				for(bool bit: input) {
+					if(p == 0.0) {
+						ew.write(intervalFalse);
+					} else if(p == 1.0) {
+						ew.write(intervalTrue);
+					} else {
+						ew.write(bit ? intervalTrue : intervalFalse);
+					}
+				}
+			}
+			
+			const uint64 size = buffer.str().size();
+			std::cerr << "size = " << size;
+			std::cerr << " " << entropy;
+			std::cerr <<  "\n"
+			
+			// Decode
+			buffer.seekg(0);
+			{
+				EntropyReader er(buffer);
+				for(bool bit: input) {
+					CHECK_EQUAL(false, er.eof());
+					const bool value = intervalTrue.includes(er.read());
+					if(p == 0.0) {
+						CHECK_EQUAL(false, value);
+					} else if(p == 1.0) {
+						CHECK_EQUAL(true, value);
+					} else {
+						CHECK_EQUAL(bit, value);
+					}
+					er.read(bit ? intervalTrue : intervalFalse);
+				}
+				CHECK_EQUAL(true, er.eof());
 			}
 		}
-		
-		// Decode
-		buffer.seekg(0);
-		{
-			EntropyReader er(buffer);
-			for(bool bit: input) {
-				CHECK_EQUAL(false, er.eof());
-				const bool value = intervalTrue.includes(er.read());
-				CHECK_EQUAL(bit, value);
-				er.read(bit ? intervalTrue : intervalFalse);
-			}
-			CHECK_EQUAL(true, er.eof());
-		}
 	}
-}
-
-TEST(Empty)
-{
-	const Inputs inputs = generate(0);
-	test(inputs, 0.1);
-	test(inputs, 0.125);
-	test(inputs, 0.2);
-	test(inputs, 0.25);
-	test(inputs, 0.3);
-	test(inputs, 0.4);
-	test(inputs, 0.5);
-	test(inputs, 0.6);
-	test(inputs, 0.75);
-	test(inputs, 0.7);
-	test(inputs, 0.8);
-	test(inputs, 0.9);
-}
-
-TEST(One)
-{
-	const Inputs inputs = generate(1);
-	test(inputs, 0.1);
-	test(inputs, 0.125);
-	test(inputs, 0.2);
-	test(inputs, 0.25);
-	test(inputs, 0.3);
-	test(inputs, 0.4);
-	test(inputs, 0.5);
-	test(inputs, 0.6);
-	test(inputs, 0.75);
-	test(inputs, 0.7);
-	test(inputs, 0.8);
-	test(inputs, 0.9);
-}
-
-TEST(Two)
-{
-	const Inputs inputs = generate(2);
-	test(inputs, 0.1);
-	test(inputs, 0.125);
-	test(inputs, 0.2);
-	test(inputs, 0.25);
-	test(inputs, 0.3);
-	test(inputs, 0.4);
-	test(inputs, 0.5);
-	test(inputs, 0.6);
-	test(inputs, 0.75);
-	test(inputs, 0.7);
-	test(inputs, 0.8);
-	test(inputs, 0.9);
-}
-
-TEST(Three)
-{
-	const Inputs inputs = generate(3);
-	test(inputs, 0.1);
-	test(inputs, 0.125);
-	test(inputs, 0.2);
-	test(inputs, 0.25);
-	test(inputs, 0.3);
-	test(inputs, 0.4);
-	test(inputs, 0.5);
-	test(inputs, 0.6);
-	test(inputs, 0.75);
-	test(inputs, 0.7);
-	test(inputs, 0.8);
-	test(inputs, 0.9);
-}
-
-TEST(Eight)
-{
-	const Inputs inputs = generate(8);
-	test(inputs, 0.1);
-	test(inputs, 0.125);
-	test(inputs, 0.2);
-	test(inputs, 0.25);
-	test(inputs, 0.3);
-	test(inputs, 0.4);
-	test(inputs, 0.5);
-	test(inputs, 0.6);
-	test(inputs, 0.75);
-	test(inputs, 0.7);
-	test(inputs, 0.8);
-	test(inputs, 0.9);
 }
 
 }
---
+++ src/Test.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+
+// Generate all the symbols bit strings upto and including length n
+std::vector<std::vector<bool>> generate_bitstrings(uint length);
+
+extern const std::vector<double> probabilities;
