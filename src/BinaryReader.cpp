c1f76835e8fdbd66db3d0b24a235406cc07c3df4
Remco Bloemen <remco@coblue.eu>
2016-02-04 19:25:11 +0100

    BinaryReader: eof on last one bit

--- src/BinaryReader.cpp
+++ src/BinaryReader.cpp
@@ -10,7 +10,11 @@ BinaryReader::BinaryReader(std::istream& input)
 
 bool BinaryReader::eof() const
 {
-	return position == 0 && in.eof();
+	// We can not end on zero.
+	static_assert(buffer_size == 8, "This code assumes one byte buffers.");
+	const uint8_t remaining_mask = mask == 0 ? 0 : mask | (mask - 1);
+	const bool bits_remaing_in_buffer = (buffer & remaining_mask) != 0;
+	return !bits_remaing_in_buffer && in.eof();
 }
 
 bool BinaryReader::read_bit()
@@ -19,14 +23,13 @@ bool BinaryReader::read_bit()
 		throw std::runtime_error("Can not read beyond end of file.");
 	}
 	
-	if(position == 0) {
+	if(mask == 0) {
 		buffer = in.get();
 		in.peek();
-		position = buffer_size;
+		mask = 1UL << (buffer_size - 1);
 	}
 	
-	bool bit = buffer >> (buffer_size - 1);
-	buffer <<= 1;
-	--position;
+	bool bit = (buffer & mask) != 0;
+	mask >>= 1;
 	return bit;
 }
--- src/BinaryReader.test.cpp
+++ src/BinaryReader.test.cpp
@@ -14,11 +14,9 @@ string read(const string input, uint symbols)
 	CHECK(in.good());
 	{
 		BinaryReader br(in);
-		while(symbols--) {
-			CHECK(!br.eof());
+		while(!br.eof()) {
 			out.append(br.read_bit() ? "1" : "0");
 		}
-		CHECK(br.eof());
 	}
 	CHECK(in.eof());
 	return out;
@@ -27,12 +25,29 @@ string read(const string input, uint symbols)
 TEST(Simple)
 {
 	CHECK_EQUAL("", read("", 0));
+	CHECK_EQUAL("1", read("\x80", 0));
+	CHECK_EQUAL("01", read("\x40", 0));
+	CHECK_EQUAL("11", read("\xC0", 0));
+	CHECK_EQUAL("001", read("\x20", 0));
+	CHECK_EQUAL("011", read("\x60", 0));
+	CHECK_EQUAL("101", read("\xA0", 0));
+	CHECK_EQUAL("111", read("\xE0", 0));
+	CHECK_EQUAL("0001", read("\x10", 0));
+	CHECK_EQUAL("0011", read("\x30", 0));
+	CHECK_EQUAL("0101", read("\x50", 0));
+	CHECK_EQUAL("0111", read("\x70", 0));
+	CHECK_EQUAL("1001", read("\x90", 0));
+	CHECK_EQUAL("1011", read("\xB0", 0));
+	CHECK_EQUAL("1101", read("\xD0", 0));
+	CHECK_EQUAL("1111", read("\xF0", 0));
+	CHECK_EQUAL("00001", read("\x08", 0));
+	CHECK_EQUAL("00011", read("\x18", 0));
 	CHECK_EQUAL("11111111", read("\xFF", 8));
 }
 
 TEST(MultiByte)
 {
-	CHECK_EQUAL("000000010010001101000000", read("\x01\x23\x40", 24));
+	CHECK_EQUAL("000000010010001101000001", read("\x01\x23\x41", 24));
 }
 
 }
